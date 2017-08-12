923f0179f539b8da3fd0f15c5c2b4adc5105d392
aurgiyalgo <tenonte3@gmail.com>
2017-08-12 12:36:51 +0100

    Committed SlimefunNukes project

---
+++ SlimefunNukes/src/com/Joselu/NukeAddon/Commands.java
@@ -0,0 +1,23 @@
+package com.Joselu.NukeAddon;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Commands implements CommandExecutor{
+
+	Main plugin;
+	
+	public Commands(Main instance) {
+		
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (label.equalsIgnoreCase("snreload")) {
+			sender.sendMessage(Main.prefix + ChatColor.GREEN + "Command disabled at the moment");
+		}
+		return true;
+	}
+}
